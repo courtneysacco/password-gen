c49fe28d860c0fa6216e7e2bebd69bc719ebf87b
courtney <xoc0urtney14xo@aim.com>
2022-10-28 17:14:16 -0400

    created readme

--- script.js
+++ script.js
@@ -170,18 +170,10 @@ function generatePassword(){
   return password;
 }
 
-
-
-
-
-
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
